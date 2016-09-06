b73c6af9a7ffccf3668a2788ce6bbf74b814e450
zli <zli@zlis-macbook-pro.pc.spawar.navy.mil>
2016-09-06 08:14:45 -1000

    wrap up tests

--- test/test_operations/test_translator.py
+++ test/test_operations/test_translator.py
@@ -13,10 +13,25 @@ class TranslatorTestCase(unittest.TestCase):
     self.translator = Translator(data)
 
   def test_translator_can_translate_data(self):
-    self.assertEqual(
-      self.translator.translate()['dataset_data']['data'],
-      self.desired_result['dataset_data']['data']
+    self.assertTrue(
+      set(self.translator.translate()['dataset_data']['data'][0]) ==
+      set(self.desired_result['dataset_data']['data'][0])
     )
+
+  def test_translator_can_keep_data_title_and_value_in_the_same_order(self):
+    self._check_data_attribute_order("supply-chain")
+    self._check_data_attribute_order("booking-no")
+    self._check_data_attribute_order("container-id")
+    self._check_data_attribute_order("gs-id")
+    self._check_data_attribute_order("msg-seq-no")
+    self._check_data_attribute_order("acq-dt")
+    self._check_data_attribute_order("msg-received-dt")
+    self._check_data_attribute_order("msg-received-age")
+    self._check_data_attribute_order("position-lat-long")
+    self._check_data_attribute_order("position-type")
+    self._check_data_attribute_order("locode")
+    self._check_data_attribute_order("msg-type")
+    self._check_data_attribute_order("raw")
 
   def test_translator_can_translate_column_names(self):
     self.assertTrue(
@@ -74,5 +89,13 @@ class TranslatorTestCase(unittest.TestCase):
     self.assertEqual(
       self.translator.translate()['dataset_data']['order'],
       self.desired_result['dataset_data']['order'],
+    )
+
+  def _check_data_attribute_order(self, attribute):
+    expected_index = self.translator.translate()['dataset_data']['column_names'].index(attribute)
+    fixture_index = self.desired_result['dataset_data']['column_names'].index(attribute)
+    self.assertEqual(
+      self.translator.translate()['dataset_data']['data'][0][expected_index],
+      self.desired_result['dataset_data']['data'][0][fixture_index]
     )
 
